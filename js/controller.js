ef0664a2f0210052406920988b4077b0737af32b
Willie Taing <taingw4@uw.edu>
2013-11-15 15:52:29 -0800

    Complete with post cart to JSON option

--- js/controller.js
+++ js/controller.js
@@ -39,7 +39,20 @@ $(function(){
 	});	
 
 	$('.place-order').click(function() {
-		console.log(cartModel.toJSON());
+		$.ajax({
+	    url: 'https://courses.washington.edu/info343/ajax/movies/orders/',
+	    type: 'POST',
+	    data: cartModel.toJSON(),
+	    contentType: 'application/json',
+	    success: function(responseData) {
+	    	alert(responseData.message); 	   
+	    	cartModel.setItems([]);
+	    },
+	    error: function(jqXHR, status, errorThrown) {
+	        //error with post--alert user
+	        alert(errorThrown || status);
+    }
+}); //ajax()
 	});
 }); //doc ready()
 
